20c78e74a9105f93ced1db96acfda075b1ecd554
MattHuntebrinker <HuntebrinkerMatt@gmail.com>
2021-12-21 16:52:43 -0700

    working locally with sql

--- main.py
+++ main.py
@@ -1,21 +1,39 @@
 import discord
 import os
 import json
+
+from pymysql.cursors import Cursor
 from bot import bot
 import pandas as pd
 import csv
 import random
+import pymysql
 import re
 
-with open('config.json') as f:
-    creds = json.load(f)
 
+
+# FUTURE HIT YOUTUBE API AND GET VIDEO DETAILS
 
 
 class handler():
     def __init__(self):
+        with open('config.json') as f:
+            self.creds = json.load(f)
+        self.token = self.creds['token']
+
         self.bot = bot()
-        self.videoList = list(pd.read_csv('videoList.csv'))
+        self.db = pymysql.connect(host=self.creds['endpoint'], user=self.creds['user'], password=self.creds['db_pass'])
+        self.cursor = self.db.cursor()
+        self.cursor.execute('''select now()''')
+        print(self.cursor.fetchall())
+        self.cursor.execute('''use vault_db''')
+        print(self.cursor.fetchall())
+        self.cursor.execute('''show tables''')
+        print(self.cursor.fetchall())
+        self.cursor.execute('''select * from vault_db.vault''')
+        print(self.cursor.fetchall())
+
+
 
     
     def receive(self, message):
@@ -24,7 +42,7 @@ class handler():
             url = message.content.split("add",1)[1]
             m = re.search(r"[a-zA-Z0-9_-]{11}$", str(message.content))
             if m and 'watch?v=' in url:
-                self.add_video(message, url)
+                self.add_video(url, name)
                 return f'Thanks {name}, that shit has been added to the vault'
             else:
                 return f'Hey {name}, you shitbag, make sure the video you enter is legit'
@@ -33,15 +51,20 @@ class handler():
         elif message.content.startswith('$vault random'):
             return self.random_video()
 
-    def add_video(self, message, url):
-        self.videoList.append(url)
-        with open('videoList.csv','a') as fd:
-            fd.write(url)
+    def add_video(self, url, name):
+        self.cursor.execute("INSERT IGNORE INTO vault(url) VALUES('{0}')".format(url.strip()))
+        print(self.cursor.fetchall())
+        self.db.commit()
+        
         #add success or failure
         
 
     def random_video(self):
-        return random.choice(self.videoList)
+        query = '''select distinct url from vault_db.vault'''
+        sql_response = pd.read_sql(query, self.db)
+        url_list = sql_response['url'].tolist()
+        print(sql_response['url'].tolist())
+        return random.choice(url_list)
 
     def delete_video(self):
         pass
@@ -69,4 +92,4 @@ async def on_message(message):
     #     await message.channel.send('Hello!')
 
 
-client.run(creds['token'])
+client.run(handler.token)
